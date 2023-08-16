fb8e6748307800f5b0b8f6077eedb9a21aa6a65c
felix <felix@zaslavskiy.net>
2023-08-16 18:19:05 -0400

    Push metadata to models.json

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # meta_llm_leaderboard
 Meta leaderboard combines data from multiple different leaderboards about LLMs.
+The focus is primarily on Open LLMs.
 
 Todo:
-- Re-score the HF leaderboard with Truth having lower value.
 - Get manual size from model.json
 - OpenCompass has mapping by Name... have it handled via model.json
+- Add to automated change tracker for AgentBench
 - Create space to show all the diagrams.
-- Add AgentBench leaderboard.
 
 Ideas:
 - Create across everything chart. Show model that has been evaluated on all leaderboards.
 - Add a runner-up (commercial permissible) license to HF best models.
 - Work on Supernatural Instruction
-- Take another look at HELM
+- Take another look at HELM (They hardly evaluate recent open models. Not even bother with finetunes)
 - https://gpt4all.io/index.html is that Test set worth it?
--- scripts/extract_alpacaeval.py
+++ scripts/extract_alpacaeval.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 from change_tracker import load_previous_data, save_current_data, track_changes
+from models import Model
+
 # Load the data
 url = "https://github.com/tatsu-lab/alpaca_eval/raw/main/docs/alpaca_eval_gpt4_leaderboard.csv"
 df = pd.read_csv(url)
@@ -12,25 +14,14 @@ df = df[df['link'].str.startswith("https://huggingface.co/") |
 
 # Extract the Model_ID from the Link column, with special handling for the arXiv link
 def extract_model_id(link, name):
-    if link == "https://ai.meta.com/llama/" and name == 'LLaMA2 Chat 70B':
-        return "meta-llama/Llama-2-70b-chat-hf"
-    elif link == "https://ai.meta.com/llama/" and name == 'LLaMA2 Chat 13B':
-        return "meta-llama/Llama-2-13b-chat-hf"
-    elif link == "https://ai.meta.com/llama/" and name == 'LLaMA2 Chat 7B':
-        return "meta-llama/Llama-2-13b-chat-hf"
-    elif link == "https://github.com/imoneoi/openchat" and name == 'OpenChat V3.1 13B':
-        return "openchat/openchat_v3.1"
-    elif link == "https://github.com/imoneoi/openchat" and name == 'OpenChat V2-W 13B':
-        return "openchat/openchat_v2_w"
-    elif link == "https://github.com/imoneoi/openchat" and name == 'OpenChat V2 13B':
-        return "openchat/openchat_v2"
-    elif link == "https://github.com/imoneoi/openchat" and name == 'OpenChat-13B':
-        return "openchat/openchat"
-    elif link == "https://github.com/imoneoi/openchat" and name == 'OpenChat8192-13B':
-        return "oopenchat/openchat_8192"
-    elif link == "https://github.com/imoneoi/openchat" and name == 'OpenCoderPlus-15B':
-        return "openchat/opencoderplus"
-    return link.replace("https://huggingface.co/", "")
+    if not link.startswith("https://huggingface.co/"):
+        model = Model.find_by_alias("../static_data/models.json", name)
+        if model is None:
+            return 'other'
+        else:
+            return model.HF_ID
+    else:
+        return link.replace("https://huggingface.co/", "")
 
 df['Model_ID'] = df.apply(lambda row: extract_model_id(row['link'], row['name']), axis=1)
 
